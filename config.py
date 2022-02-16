8d484df33e17028125b7a8281aebd6c41bf10ebe
Tobias Schiolborg <tschiolborg@gmail.com>
2022-02-16 17:26:40 +0100

    let's go

---
+++ config.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+from typing import List, Dict
+
+
+@dataclass
+class DatasetConfig:
+    use_single_annotation_file: bool
+    anno_dir: str
+    img_dir: str
+    classes: List[str]
+    label_map: Dict[str, int]
+
+
+@dataclass
+class DataloaderConfig:
+    shuffle: bool
+    batch_size: int
+    num_workers: int
+
+
+@dataclass
+class BackboneConfig:
+    name: str
+    fpn_compatible: bool
+
+
+@dataclass
+class ModelConfig:
+    name: str
+    num_classes: int
+    weights: str
+
+
+@dataclass
+class TrainConfig:
+    epochs: int
+    lr: float
+    dataloader: DataloaderConfig
+    dataset: DatasetConfig
+
+
+@dataclass
+class DevConfig:
+    dataset: DatasetConfig
+    dataloader: DataloaderConfig
+
+
+@dataclass
+class PipelineConfig:
+    train: TrainConfig
+    dev: DevConfig
+    model: ModelConfig
+
+
+
