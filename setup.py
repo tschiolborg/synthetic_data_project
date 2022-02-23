5ade8105ae5eecc2d37da758af8db1c72852724b
Tobias Schiolborg <tschiolborg@gmail.com>
2022-02-23 11:23:39 +0100

    cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using synthetic data tto train deep learning models for object detection of traffic signs',
+    author='Tobias Schiolborg',
+    license='MIT',
+)
